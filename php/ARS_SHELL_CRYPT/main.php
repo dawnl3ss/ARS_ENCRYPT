71e492e36d3003926b51488df80a2ab144e6480c
Neptune <52249849+Neptune-Dev@users.noreply.github.com>
2021-04-29 08:27:00 +0200

    str_encrypt

--- php/ARS_SHELL_CRYPT/main.php
+++ php/ARS_SHELL_CRYPT/main.php
@@ -2,6 +2,6 @@
 
 require_once "cryptage/Encryption.php";
 
-$encrypt_sec_1 = new Encryption("sale merde", Encryption::STANDARD_SPACING_SEC_1);
+$encrypt_sec_1 = new Encryption("string_to_encrypt", Encryption::STANDARD_SPACING_SEC_1);
 echo $encrypt_sec_1->str_encrypt() . "<br>";
 echo $encrypt_sec_1->str_decrypt();
