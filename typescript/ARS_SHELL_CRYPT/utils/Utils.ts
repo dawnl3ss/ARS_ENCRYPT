df35fe7111dd000689808545fce298f9a317f159
Galzronn <69150061+Galzronn@users.noreply.github.com>
2021-05-02 14:11:45 +0200

    remove useless class

--- typescript/ARS_SHELL_CRYPT/utils/Utils.ts
+++ typescript/ARS_SHELL_CRYPT/utils/Utils.ts
@@ -1,26 +1,23 @@
-export class Utils {
+/**
+ * Get random number between the min value and the max value
+ * @param min
+ * @param max
+ */
+export function mt_rand(min: number, max: number) : number;
+export function mt_rand() : number;
 
-    /**
-     * Get random number between the min value and the max value
-     * @param min
-     * @param max
-     */
-    static mt_rand(min = 0, max = 2147483647) {
-        if(arguments.length === 1) {
-            throw new Error("Warning: mt_rand() expects exactly 2 parameters, 1 given")
-        }
-
-        min = parseInt(String(min), 10)
-        max = parseInt(String(max), 10)
-
-        return Math.floor(Math.random() * (max - min + 1)) + min
+export function mt_rand(min?: number, max?: number): number {
+    if (typeof min === 'undefined' || typeof max === 'undefined') {
+        min = 0;
+        max = 2147483647;
     }
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
-    /**
-     * String reverse
-     * @param str
-     */
-    static reverse(str: string) {
-        return str.split("").reverse().join("");
-    }
+/**
+ * String reverse
+ * @param str
+ */
+export function reverse(str: string) : string {
+    return str.split("").reverse().join("");
 }
