d09a07ecbe13b91a30f8d7f60cfe310cf80bb76a
Galzronn <69150061+Galzronn@users.noreply.github.com>
2021-05-02 14:18:22 +0200

    Adding the results as a comment

--- typescript/ARS_SHELL_CRYPT/main.ts
+++ typescript/ARS_SHELL_CRYPT/main.ts
@@ -3,5 +3,5 @@ import { StandardSecurity } from "./standard/StandardSecurity";
 
 const encrypt = new Encryption("azerty", StandardSecurity.STANDARD_SEC_1);
 
-console.log(encrypt.str_encrypt());
-console.log(encrypt.str_decrypt());
+console.log(encrypt.str_encrypt()); //f9jwy7`(m.vnFCpx/7ywj9f`q=TfxR+M23
+console.log(encrypt.str_decrypt()); //azerty
