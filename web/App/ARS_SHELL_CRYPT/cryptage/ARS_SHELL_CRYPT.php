a68fad9915fae285440e658d3dd679736f373bfa
Neptune <52249849+Neptune-Dev@users.noreply.github.com>
2021-05-12 21:36:43 +0200

    ars-crypter web

---
+++ web/App/ARS_SHELL_CRYPT/cryptage/ARS_SHELL_CRYPT.php
@@ -0,0 +1,79 @@
+<?php
+
+include("./standard/StandardList.php");
+include("./standard/StandardSecurity.php");
+
+class ARS_SHELL_CRYPT implements StandardList, StandardSecurity {
+
+    public const METHOD_CRYPT = 0;
+    public const METHOD_DECRYPT = 1;
+
+    /**
+     * @param string $f_char
+     *
+     * @return string
+     */
+    public function char_to_achar(string $f_char, int $method, int $_spacing) : string {
+        $n_char = "";
+
+        if ($method === self::METHOD_CRYPT) {
+            for ($i = 0; $i <= $this->count() - 1; $i++) {
+                if (self::CHAR_LIST[$i] === $f_char) {
+                    if ($i >= $_spacing) {
+                        do {
+                            if (isset(self::CHAR_LIST[$i + $_spacing])) {
+                                $n_char = self::CHAR_LIST[$i + $_spacing];
+                                break;
+                            } elseif (isset(self::CHAR_LIST[($i + $_spacing) - $this->count()])) {
+                                $n_char = self::CHAR_LIST[($i + $_spacing) - $this->count()];
+                                break;
+                            }
+                        } while (true);
+                        break;
+                    } elseif ($i < $_spacing) {
+                        do {
+                            if (isset(self::CHAR_LIST[$i + $_spacing])) {
+                                $n_char = self::CHAR_LIST[$i + $_spacing];
+                                break;
+                            }
+                        } while (true);
+                        break;
+                    }
+                }
+            }
+        } elseif ($method === self::METHOD_DECRYPT){
+            for ($i = 0; $i <= (int)$this->count() - 1; $i++) {
+                if (self::CHAR_LIST[$i] === $f_char) {
+                    if ($i >= $_spacing) {
+                        do {
+                            if (isset(self::CHAR_LIST[$i - $_spacing])) {
+                                $n_char = self::CHAR_LIST[$i - $_spacing];
+                                break;
+                            }
+                        } while (true);
+                        break;
+                    } elseif ($i < $_spacing) {
+                        do {
+                            if (isset(self::CHAR_LIST[$i - $_spacing])) {
+                                $n_char = self::CHAR_LIST[$i - $_spacing];
+                                break;
+                            } elseif (isset(self::CHAR_LIST[($i - $_spacing) + $this->count()])) {
+                                $n_char = self::CHAR_LIST[($i - $_spacing) + $this->count()];
+                                break;
+                            }
+                        } while (true);
+                        break;
+                    }
+                }
+            }
+        }
+        return $n_char;
+    }
+
+    /**
+     * @return int
+     */
+    public function count() : int {
+        return count(self::CHAR_LIST);
+    }
+}
