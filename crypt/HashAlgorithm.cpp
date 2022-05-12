c3b903fea1f112d851afea39d99a032af2fde60a
Neptune <52249849+Neptune-IT@users.noreply.github.com>
2022-05-12 23:26:18 +0200

    update [0.0.5] : introducing hash-algorithm

---
+++ crypt/HashAlgorithm.cpp
@@ -0,0 +1,13 @@
+class HashAlgorithm {
+
+public:
+    std::string hash_char(){
+        //TODO
+        return "";
+    }
+
+    std::string unhash_char(){
+        //TODO
+        return "";
+    }
+};
