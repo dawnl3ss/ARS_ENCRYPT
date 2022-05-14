576348c9ac6109ff7016c91ea07e49e7508b6e01
Neptune-IT <neptune.dev.cybsec@gmail.com>
2022-05-15 00:42:25 +0200

    update [1.0.0] : rewrite complete

--- ars.h
+++ ars.h
@@ -1,6 +1,10 @@
 #ifndef ARS_SHELL_CRYPT_ARS_H
 #define ARS_SHELL_CRYPT_ARS_H
 
+#include "src/standard/standard_security.cpp"
+
+struct standard_security security;
+
 std::string encrypt(std::string plaintext, int sec);
 std::string decrypt(std::string hash, int sec);
 
--- example.cpp
+++ example.cpp
@@ -3,11 +3,11 @@
 
 int main(){
     // Test encrypt :
-    std::string encrypt_text = encrypt("test", 2);
+    std::string encrypt_text = encrypt("test", security.STANDARD_SEC_1);
     std::cout << encrypt_text << std::endl;
 
     // Test decrypt :
-    std::string decrypt_text = decrypt(encrypt_text, 2);
+    std::string decrypt_text = decrypt(encrypt_text, security.STANDARD_SEC_1);
     std::cout << decrypt_text << std::endl;
     return 0;
 }
