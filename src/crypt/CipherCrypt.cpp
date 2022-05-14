2c07ac44a0a0f23b9524d0e51714bc1fffecd140
Neptune-IT <neptune.dev.cybsec@gmail.com>
2022-05-14 22:52:36 +0200

    update [0.4.0] : separate class into h files [cipher-crypt]

--- src/crypt/CipherCrypt.cpp
+++ src/crypt/CipherCrypt.cpp
@@ -1,43 +1,31 @@
-#include "../security/SecurityHandler.cpp"
-#include "../../utils/Utils.cpp"
+#include "CipherCrypt.hpp"
 
-class CipherCrypt : public SecurityHandler {
+CipherCrypt::CipherCrypt(std::string plaintext, int sec, std::string encrypted, std::string decrypted):
+    plaintext(plaintext),
+    sec(sec),
+    encrypted(encrypted),
+    decrypted(decrypted)
+{}
 
-private:
-    std::string
-        plaintext = "",
-        encrypted = "",
-        decrypted = "";
-    int sec = 0;
+std::string CipherCrypt::encrypt(){
+    std::string encrypt_s = "";
 
-public:
-    CipherCrypt(std::string plaintext, int sec, std::string encrypted = "", std::string decrypted = ""){
-        this->plaintext = plaintext;
-        this->sec = sec;
-        this->encrypted = encrypted;
-        this->decrypted = decrypted;
-        this->serialize_data(sec, plaintext.length());
+    for (int i = 0; i < this->plaintext.length(); i++){
+        encrypt_s += this->hash_char(std::string(1, this->plaintext.at(i)), this->_s);
     }
+    this->encrypted = encrypt_s + this->add + this->f_break + this->s_break;
+    return this->encrypted;
+}
 
-    std::string encrypt(){
-        std::string encrypt_s = "";
+std::string CipherCrypt::decrypt(){
+    std::string decrypt_s = "";
+    std::vector<std::string> part = explode(this->encrypted, '.');
+    std::string hash = part.at(0);
 
-        for (int i = 0; i < this->plaintext.length(); i++){
-            encrypt_s += this->hash_char(std::string(1, this->plaintext.at(i)), this->_s);
-        }
-        this->encrypted = encrypt_s + this->add + this->f_break + this->s_break;
-        return this->encrypted;
+    for (int i = 0; i < hash.size(); i++){
+        decrypt_s += this->unhash_char(std::string(1, hash.at(i)), this->_s);
     }
+    this->decrypted = decrypt_s;
+    return this->decrypted;
+}
 
-    std::string decrypt(){
-        std::string decrypt_s = "";
-        std::vector<std::string> part = explode(this->encrypted, '.');
-        std::string hash = part.at(0);
-
-        for (int i = 0; i < hash.size(); i++){
-            decrypt_s += this->unhash_char(std::string(1, hash.at(i)), this->_s);
-        }
-        this->decrypted = decrypt_s;
-        return this->decrypted;
-    }
-};
---
+++ src/crypt/CipherCrypt.hpp
@@ -0,0 +1,22 @@
+#ifndef ARS_SHELL_CRYPT_CIPHERCRYPT_H
+#define ARS_SHELL_CRYPT_CIPHERCRYPT_H
+
+#include "../security/SecurityHandler.cpp"
+#include "../../utils/Utils.cpp"
+
+class CipherCrypt : public SecurityHandler {
+
+private:
+    std::string
+        plaintext = "",
+        encrypted = "",
+        decrypted = "";
+    int sec = 0;
+
+public:
+    CipherCrypt(std::string plaintext, int sec, std::string encrypted, std::string decrypted);
+    std::string encrypt() const;
+    std::string decrypt() const;
+};
+
+#endif
